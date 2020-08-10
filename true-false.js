7a23b571a7258c7709a974a9d38c26c0f2688bd2
AyeshaWebHero <ayeshanusratnova@gmail.com>
2020-08-10 20:06:42 +0600

    true vs. false values

---
+++ true-false.js
@@ -0,0 +1,41 @@
+// const age = 5;
+
+// if(age){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Conditon is false");
+// }
+
+// const name = "";
+
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Conditon is false");
+// }
+
+
+// const name = " ";
+
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Conditon is false");
+// }
+
+let name = null;
+console.log(name);
+
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Conditon is false");
+}
+//0
+//""
+//undefined
+//null --- these are falsy values 
